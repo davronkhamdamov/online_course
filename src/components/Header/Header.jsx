b8eb73c03c31a37ce4a0937e699c992b36cb63ae
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-08-07 20:49:46 +0500

    Header complated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,24 @@
-export const Header = () => {
-  return <></>
+import "./Header.css"
+const Header = () => {
+  return (
+    <header>
+      <div className="d-flex align-items-center gap-4 w-75">
+        <div className="d-flex align-items-center justify-content-center">
+          <p className="icon_text">Online course</p>
+        </div>
+        <div className="categories">
+          <p>Categories</p>
+        </div>
+        <div className="searchWrapper">
+          <input type="text" placeholder="Search anything" />
+        </div>
+      </div>
+      <div className="authWrapper">
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
+    </header>
+  )
 }
+
+export default Header
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,3 +1,10 @@
-export const Home = () => {
-  return <></>
+import Header from "../../components/Header/Header"
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
+export default Home
