161bf366323af69213e2309910898568173235b2
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-08-08 01:22:23 +0500

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,10 @@
-export const Footer = () => {
-  return <></>
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <footer className="position-fixed bottom-0 w-100 d-flex justify-content-center text-white">
+      <p className="mt-2">© {new Date().getFullYear()} powered By</p>
+    </footer>
+  )
 }
+
+export default Footer
